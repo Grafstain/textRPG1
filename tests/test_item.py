b5bb03c11973543d9cfda977526a156521d40ea1
grafstain <romanice@bk.ru>
2023-02-26 21:13:44 +0300

    rename and 1 cosmetic update

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,7 @@
 import unittest
 from item import Item
 
+
 class ItemTest(unittest.TestCase):
     """
     This class contains test cases for the Item class.
@@ -21,6 +22,7 @@ class ItemTest(unittest.TestCase):
         item = Item("strength", 5)
         self.assertEqual(item.effect, "strength")
         self.assertEqual(item.amount, 5)
+
 
 if __name__ == '__main__':
     unittest.main()
